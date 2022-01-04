6712197b3105f67c720a0535f181fcbdb6ef856f
Kuntal Ojha <kuntalojha2021@gmail.com>
2022-01-04 20:06:21 +0530

    update gitignore

--- C2_Programming_Basics_6_Degree_to_Fahrenheit_Convert.cpp
+++ C2_Programming_Basics_6_Degree_to_Fahrenheit_Convert.cpp
@@ -1,7 +1,13 @@
 // Created by KUNTAL OJHA on 31/07/2021.
+// Convart Degree to Fahrenheit
 #include<iostream>
 using namespace std;
 int main()
 {
-    cout << "\"Run, Stop, run,\" she said.";
+    int ftemp;  // for temperature in fahrenheit
+    cout << "Enter tmperature in fahrenheit: ";
+    cin >> ftemp;
+    int ctemp = (ftemp-32) * 5/9;
+    cout << "Eqquivalent in Celsiuse is: " << ctemp << "\n";
+    return 0;
 }
